e08216c8bcd757623df2711b1d973f6b8398d123
dudtjq <dudtjq0415@naver.com>
2024-04-11 16:54:52 +0900

    Initial commit

---
+++ going/src/main/java/com/camp/going/GoingApplication.java
@@ -0,0 +1,13 @@
+package com.camp.going;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoingApplication.class, args);
+	}
+
+}
