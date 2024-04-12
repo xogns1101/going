a1e3e7e22b7bcc6ed114bb38380c3b996b37fac1
SJHlll <ssaadd1212@gmail.com>
2024-04-12 18:50:18 +0900

    공지사항 게시글 전체조회, 단일조회, 삭제 테스트 완료

--- going/src/main/java/com/camp/going/repository/NoticeRepository.java
+++ going/src/main/java/com/camp/going/repository/NoticeRepository.java
@@ -2,11 +2,17 @@ package com.camp.going.repository;
 
 import com.camp.going.entity.Notice;
 
+import java.util.List;
+
 // 공지사항 기능
 
 public interface NoticeRepository {
 
-    // 공지사항 등록
     void save(Notice notice);
 
+    List<Notice> findAll();
+
+    Notice findOne(int noticeNo);
+
+    void delete(int noticeNo);
 }
