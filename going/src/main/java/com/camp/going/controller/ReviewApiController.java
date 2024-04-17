24a99852fc7f9c1015fd3da4b4b33a2d4389c040
sseung <duaqkr2@naver.com>
2024-04-17 16:08:33 +0900

    풀 받는데 문제가 생김

--- going/src/main/java/com/camp/going/controller/ReviewApiController.java
+++ going/src/main/java/com/camp/going/controller/ReviewApiController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/review")
 @RequiredArgsConstructor
 @Slf4j
-public class ReviewApiController {
+public class ReviewApiController { //
 
     private final ReviewService reviewService;
 
