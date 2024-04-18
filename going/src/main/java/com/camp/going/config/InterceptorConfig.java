535981b7cb42e074aac6abe0b205ebbd71fb92de
SJHlll <ssaadd1212@gmail.com>
2024-04-18 11:19:14 +0900

    리뷰 인터셉터 추가 (ReviewMapper에 fineOne이 필요)

--- going/src/main/java/com/camp/going/config/InterceptorConfig.java
+++ going/src/main/java/com/camp/going/config/InterceptorConfig.java
@@ -3,6 +3,7 @@ package com.camp.going.config;
 import com.camp.going.interceptor.AfterLoginInterceptor;
 import com.camp.going.interceptor.AutoLoginInterceptor;
 import com.camp.going.interceptor.NoticeInterceptor;
+import com.camp.going.interceptor.ReviewInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,6 +16,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     private final AfterLoginInterceptor afterLoginInterceptor;
     private final AutoLoginInterceptor autoLoginInterceptor;
     private final NoticeInterceptor noticeInterceptor;
+    private final ReviewInterceptor reviewInterceptor;
 
 
     @Override
@@ -22,11 +24,14 @@ public class InterceptorConfig implements WebMvcConfigurer {
         // 로그인 후 비회원전용 페이지 접근 차단 인터셉터 등록
         registry
                 .addInterceptor(afterLoginInterceptor) // 어떤 인터셉터를 등록할 것인가
-                .addPathPatterns("user/sign-in", "user/sign-up"); // 어디서 들어온 요청을 강하게
+                .addPathPatterns("/user/sign-in", "/user/sign-up"); // 어디서 들어온 요청을 강하게
         registry
                 .addInterceptor(noticeInterceptor)
                 .addPathPatterns("/notice/*")
                 .excludePathPatterns("/notice/list", "/notice/detail");
+        registry
+                .addInterceptor(reviewInterceptor)
+                .addPathPatterns("/review/write");
 
 
         // 자동 로그인 인터셉터 설정
