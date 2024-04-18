2993eee77474c36c5146bb10584eae74f8f6ecee
sseung <duaqkr2@naver.com>
2024-04-18 11:10:33 +0900

    reviewController / DTO / reviewService 수정 중

---
+++ going/src/main/java/com/camp/going/dto/request/ReviewRequestDTO.java
@@ -0,0 +1,18 @@
+package com.camp.going.dto.request;
+
+import com.camp.going.entity.Review;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Setter @Getter @ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class ReviewRequestDTO {
+
+    private String content; // 리뷰 내용
+    private String email; // 리뷰 작성자
+    private String image;
+
+}
