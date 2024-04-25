a83e10ca7a4fc9157f70f76d9c64981e434b6132
dudtjq <dudtjq0415@naver.com>
2024-04-25 15:28:44 +0900

    회원 가입 로그인 됩니다

--- going/src/main/java/com/camp/going/entity/Reservation.java
+++ going/src/main/java/com/camp/going/entity/Reservation.java
@@ -10,13 +10,11 @@ import java.time.LocalDate;
 public class Reservation {
 
     private int reservationNumber;
+    private int userId;
     private int campId; // 캠핑장 고유 번호
-    private String campName; // 캠핑장 이름
-    private String email; // 사용자 이메일 (아이디)
     private LocalDate regDate; // 최초 예약 날짜
     private LocalDate regDates; // 퇴실 예약 날짜
-    private int price; // 캠핑장 가격
-    private String phoneNumber; //  사용자 전화 번호
+
 
 
 
